b21777985c320fec16937eea0956a491d4020131
Amila <amila.jayakody@outlook.com50618523+AmilaDarshanaJ@users.noreply.github.com>
2025-11-13 08:59:34 +0530

    Run migrations for ActivityLand scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_25_035825) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_13_032645) do
+  create_table "activities", force: :cascade do |t|
+    t.string "land"
+    t.date "start_date"
+    t.date "end_date"
+    t.text "summary"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "harvests", force: :cascade do |t|
     t.integer "land_id", null: false
     t.date "planned_date"
