9d4b2d9db1f0048d003d038e33d16b4babebc5b6
Amila <amila.jayakody@outlook.com50618523+AmilaDarshanaJ@users.noreply.github.com>
2025-11-19 09:13:08 +0530

    Add Top Navigation bar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menu = document.querySelector(".nav-menu");
+    const toggle = document.querySelector("#mobile-menu");
+
+    toggle.addEventListener("click", () => {
+        menu.classList.toggle("active");
+    });
+});
+
