0096fe484318bac60960eb9233ae36ee0ee36f39
Amila <amila.jayakody@outlook.com50618523+AmilaDarshanaJ@users.noreply.github.com>
2025-11-20 11:29:44 +0530

    add top navigation bar with Login button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   include Authentication
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+
+  helper_method :current_user, :user_signed_in?
+
   allow_browser versions: :modern
 end
